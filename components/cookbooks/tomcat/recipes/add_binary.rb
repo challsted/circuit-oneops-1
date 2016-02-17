5d80408eb5730ba4c3edebbf023016734f02edc9
Chase Hallsted <chase.hallsted@gmail.com>
2016-02-16 21:10:24 -0600

    Passing Most RuboCop and FC stuff, needs refactoring

--- components/cookbooks/tomcat/recipes/add_binary.rb
+++ components/cookbooks/tomcat/recipes/add_binary.rb
@@ -5,7 +5,7 @@
 # mirrors attribute is an array of uri's prefixing the distribution path convention
 
 major_and_minor = node['tomcat']['version']
-major_version = major_and_minor.gsub(/\..*/,"")
+major_version = major_and_minor.gsub(/\..*/, '')
 
 build_version = node['tomcat']['build_version']
 full_version = "#{major_and_minor}.#{build_version}"
@@ -24,36 +24,36 @@ end
 dest_file = "#{node['tomcat']['tomcat_install_dir']}/apache-tomcat-#{full_version}.tar.gz"
 
 source_list = JSON.parse(node['tomcat']['mirrors']).map! { |mirror| "#{mirror}/#{tarball}" }
-##Get apache mirror configured for the cloud, if no mirror is defined for component.
+# Get apache mirror configured for the cloud, if no mirror is defined for component.
 if source_list.empty?
   cloud_name = node['workorder']['cloud']['ciName']
   services = node['workorder']['services']
 
-  if services.nil? || !services.has_key?(:mirror)
+  if services.nil? || !services.key?(:mirror)
     Chef::Log.error("Please make sure  cloud '#{cloud_name}' has mirror service with 'apache' eg {apache=>http://archive.apache.org/dist}")
     exit 1
   end
   mirrors = JSON.parse(services[:mirror][cloud_name][:ciAttributes][:mirrors])
-  if mirrors.nil? || !mirrors.has_key?('apache')
+  if mirrors.nil? || !mirrors.key?('apache')
     Chef::Log.error("Please make sure  cloud '#{cloud_name}' has mirror service with 'apache' eg {apache=>http://archive.apache.org/dist}")
     exit 1
   end
   mirrors = JSON.parse(node['workorder']['services']['mirror'][cloud_name]['ciAttributes']['mirrors'])
-  source_list = mirrors['apache'].split(",").map { |mirror| "#{mirror}/#{tarball}" }
+  source_list = mirrors['apache'].split(',').map { |mirror| "#{mirror}/#{tarball}" }
 end
 
 build_version_checksum = {
-  "62" => "a787ea12e163e78ccebbb9662d7da78e707aef051d15af9ab5be20489adf1f6d",
-  "42" => "c163f762d7180fc259cc0d8d96e6e05a53b7ffb0120cb2086d6dfadd991c36df",
+  '62' => 'a787ea12e163e78ccebbb9662d7da78e707aef051d15af9ab5be20489adf1f6d',
+  '42' => 'c163f762d7180fc259cc0d8d96e6e05a53b7ffb0120cb2086d6dfadd991c36df'
 }
 
-shared_download_http source_list.join(",") do
+shared_download_http source_list.join(',') do
   path dest_file
   action :create
-  checksum build_version_checksum["#{build_version}"]
+  checksum build_version_checksum #{build_version}
 end
 
-tar_flags = "--exclude webapps/ROOT"
+tar_flags = '--exclude webapps/ROOT'
 execute "tar #{tar_flags} -zxf #{dest_file}" do
   cwd node['tomcat']['tomcat_install_dir']
 end
@@ -70,11 +70,11 @@ username = node['tomcat_owner']
 group = node['tomcat_group']
 
 execute "groupadd #{group}" do
-  returns [0,9]
+  returns [0, 9]
 end
 
 execute "useradd -g #{group} -d #{node['tomcat']['tomcat_install_dir']}/tomcat#{major_version} #{username}" do
-  returns [0,9]
+  returns [0, 9]
 end
 
 execute "chown -R #{username}:#{group} apache-tomcat-#{full_version}" do
@@ -82,8 +82,8 @@ execute "chown -R #{username}:#{group} apache-tomcat-#{full_version}" do
 end
 
 base_dir = "#{node['tomcat']['tomcat_install_dir']}/apache-tomcat-#{full_version}"
-node.set["tomcat"]["base"] = base_dir
-node.set["tomcat"]["home"] = base_dir
+node.set['tomcat']['base'] = base_dir
+node.set['tomcat']['home'] = base_dir
 
 cookbook_file 'custom-root-file' do
   source 'ROOT.tar.gz'
@@ -93,10 +93,10 @@ cookbook_file 'custom-root-file' do
   mode '0755'
   action :create
   notifies :create, 'directory[create-root-folder]', :immediately
-  not_if  { File.exist?("#{base_dir}/webapps/ROOT") }
+  not_if { File.exist?("#{base_dir}/webapps/ROOT") }
 end
 
-directory "create-root-folder" do
+directory 'create-root-folder' do
   path "#{base_dir}/webapps/ROOT"
   owner username
   group group
@@ -105,7 +105,7 @@ directory "create-root-folder" do
   notifies :run, 'execute[untar-custom-root-file]', :immediately
 end
 
-execute "untar-custom-root-file" do
+execute 'untar-custom-root-file' do
   command 'tar -zxf ROOT.tar.gz -C ROOT'
   cwd "#{base_dir}/webapps"
   action :nothing
@@ -113,8 +113,8 @@ execute "untar-custom-root-file" do
 end
 
 file 'delete-custom-root-file' do
-  path  "#{base_dir}/webapps/ROOT.tar.gz"
-  action  :nothing
+  path "#{base_dir}/webapps/ROOT.tar.gz"
+  action :nothing
 end
 
 directory "#{base_dir}/webapps/examples" do
@@ -127,86 +127,91 @@ directory "#{base_dir}/webapps/docs" do
   action :delete
 end
 
-template "#{base_dir}/bin/setenv.sh" do
-  source "setenv.sh.erb"
+# Food Critic reports an issue of a missing template regardless of its presence
+template "#{base_dir}/bin/setenv.sh" do # ~FC033
+  source 'setenv.sh.erb'
   owner username
   group group
-  mode "0755"
+  mode '0755'
 end
 
-if !node['tomcat']['post_startup_command'].nil?
+# TODO: Research "Chef Guards"
+unless node['tomcat']['post_startup_command'].nil? # ~FC023
   file "#{base_dir}/bin/poststartup.sh" do
-    content node["tomcat"]["post_startup_command"].gsub(/\r\n?/,"\n")
+    content node['tomcat']['post_startup_command'].gsub(/\r\n?/, "\n")
     owner username
     group group
-    mode "0755"
+    mode '0755'
   end
 end
 
-depends_on=node['workorder']['payLoad'].DependsOn.reject { |d| d['ciClassName'] !~ /Javaservicewrapper/ }
-#if the javaservicewrapper component is present, dont generate the tomcat initd
-if (depends_on.nil? || depends_on.empty? || depends_on[0][:rfcAction] == 'delete')
-  template "/etc/init.d/tomcat#{major_version}" do
-    source "generic_initd.erb"
-    mode "0755"
+depends_on = node['workorder']['payLoad'].DependsOn.reject { |d| d['ciClassName'] !~ /Javaservicewrapper/ }
+# if the javaservicewrapper component is present, dont generate the tomcat initd
+if depends_on.nil? || depends_on.empty? || depends_on[0][:rfcAction] == 'delete'
+  # Food Critic reports an issue of a missing template regardless of its presence
+  template "/etc/init.d/tomcat#{major_version}" do # ~FC033
+    source 'generic_initd.erb'
+    mode '0755'
   end
 else
-#call wrapper.configure recipe
-  include_recipe "javaservicewrapper::wire_ci_attr"
-  #Ignoring foodcritic violation(FC007) when using self as the cookbook related to https://github.com/acrmp/foodcritic/issues/44
-  include_recipe "tomcat::setwrapperattribs" # ~FC007
-  include_recipe "javaservicewrapper::configure"
+  include_recipe 'javaservicewrapper::wire_ci_attr'
+  include_recipe 'tomcat::setwrapperattribs'
+  include_recipe 'javaservicewrapper::configure'
 end
 
 template "#{base_dir}/conf/server.xml" do
   source "server#{major_version}.xml.erb"
-  owner "root"
-  group "root"
-  mode "0644"
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
 
 template "#{base_dir}/conf/web.xml" do
   source "web#{major_version}.xml.erb"
-  owner "root"
-  group "root"
-  mode "0644"
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
 
-template "#{base_dir}/conf/context.xml" do
-   source "context7.xml.erb"
-   mode "0644"
+# Food Critic reports an issue of a missing template regardless of its presence
+template "#{base_dir}/conf/context.xml" do # ~FC033
+  source 'context7.xml.erb'
+  mode '0644'
 end
 
-template "#{base_dir}/conf/tomcat-users.xml" do
-  source "tomcat-users.xml.erb"
-  owner "root"
-  group "root"
-  mode "0644"
+# Food Critic reports an issue of a missing template regardless of its presence
+template "#{base_dir}/conf/tomcat-users.xml" do # ~FC033
+  source 'tomcat-users.xml.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
 
-template "#{base_dir}/conf/manager.xml" do
-  source "manager.xml.erb"
-  owner "root"
-  group "root"
-  mode "0644"
+# Food Critic reports an issue of a missing template regardless of its presence
+template "#{base_dir}/conf/manager.xml" do # ~FC033
+  source 'manager.xml.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
 
-template "#{base_dir}/conf/catalina.policy" do
-  source "catalina.policy.erb"
-  owner "root"
-  group "root"
-  mode "0644"
+# Food Critic reports an issue of a missing template regardless of its presence
+template "#{base_dir}/conf/catalina.policy" do # ~FC033
+  source 'catalina.policy.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
 
-unless node["tomcat"]["access_log_dir"].start_with?("/")
-  node.set['tomcat']['access_log_dir'] =  "#{base_dir}/#{node['tomcat']['access_log_dir']}"
+unless node['tomcat']['access_log_dir'].start_with?('/')
+  node.set['tomcat']['access_log_dir'] = "#{base_dir}/#{node['tomcat']['access_log_dir']}"
 end
 
-#Set up the log directories
-log_dir=node["tomcat"]["logfiles_path"]
-access_log_dir=node["tomcat"]["logfiles_path"]
-Chef::Log.info("Installation type #{node["tomcat"]["install_type"]} - access log #{access_log_dir} logpath : #{log_dir}")
-[log_dir,access_log_dir].each do |dir_name|
+# Set up the log directories
+log_dir = node['tomcat']['logfiles_path']
+access_log_dir = node['tomcat']['logfiles_path']
+Chef::Log.info("Installation type #{node['tomcat']['install_type']} - access log #{access_log_dir} logpath : #{log_dir}")
+[log_dir, access_log_dir].each do |dir_name|
   directory dir_name do
     action :create
     recursive true
